ca36759e086e05b3c035b970071d97d53a3599e6
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-10-14 16:02:03 +0200

    Update GetWsOne.sh

--- GetWsOne.sh
+++ GetWsOne.sh
@@ -8,6 +8,6 @@
 GetWsone () {
     curl https://packages.vmware.com/wsone/VMwareWorkspaceONEIntelligentHub.pkg --output /Users/$USER/Downloads/VMwareWorkspaceONEIntelligentHub.pkg
     sudo installer -pkg /Users/$USER/Downloads/VMwareWorkspaceONEIntelligentHub.pkg -target /Applications
-    rm /Users/$USER/Downloads/VMwareWorkspaceONEIntelligentHub.pkg
+    # rm /Users/$USER/Downloads/VMwareWorkspaceONEIntelligentHub.pkg
 }
 GetWsone
