edb30f2493589fcb643474b79cc39bc50de3b063
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-10-14 16:46:45 +0200

    Create GetDisplayLink.sh

---
+++ GetDisplayLink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# SYNOPSIS
+#   Function download and install DisplayLink macOS Software
+#   https://getwsone.com/
+#
+
+GetDisplayLink () {
+    curl https://www.synaptics.com/sites/default/files/exe_files/2022-07/DisplayLink%20Manager%20Graphics%20Connectivity1.7.1-EXE.pkg --output /Users/$USER/Downloads/DisplayLink.pkg
+    sudo installer -pkg /Users/$USER/Downloads/DisplayLink.pkg -target /Applications
+    rm -f /Users/$USER/Downloads/DisplayLink.pkg
+}
+GetWsone
